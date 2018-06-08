b72ae233ecbf915bdaa50961732fe5680e7c53c6
Broderick Stadden <bstadden@gmail.com>
2018-06-08 09:34:34 +0100

    added margin to words area

--- src/components/Results/Words/Words.js
+++ src/components/Results/Words/Words.js
@@ -24,7 +24,7 @@ const words = (props) => {
     }];
 
     return (
-        <div >
+        <div className="words">
             <WordCount count={props.wordCount}/>
             <ReactTable 
                 data={tableData} 
